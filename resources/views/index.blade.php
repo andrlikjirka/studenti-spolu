76f126d2f554236c9bd312e2034e117e0ffedcfd
Jiří Andrlík <andrlik.jirka@gmail.com>
2022-03-21 22:54:37 +0100

    bootstrap sass, js, laravel-mix conf

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.layout')
+
+@section('content')
+    <section class="bg-primary bg-gradient" style="padding-top: 70px">
+        <div class="container px-5 py-5">
+            <div class="row gx-5 align-items-center">
+                <div class="col-lg-6 col-md-6 text-white">
+                    <h1 class="fw-bold py-3 font-monospace">studenti|spolu<sup class="">(ZČU)</sup></h1>
+                    <p class="mt-3 lh-lg">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam egestas wisi a erat. Aliquam
+                        erat volutpat. Pellentesque sapien. Aenean vel massa quis mauris vehicula lacinia. Curabitur
+                        bibendum justo.
+                    </p>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <!--<img style="width: 90%;" class="d-block m-auto" src="img/Obrázek3.png" alt="">-->
+                </div>
+            </div>
+        </div>
+    </section>
+
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="jandrlik">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>studenti.spolu</title>
+    <!--<link rel="icon" type="image/png" href="img/favicon-32x32.png">-->
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+</head>
+<body>
+
+<!-- Navbar-->
+<header>
+    <nav id="" class="navbar navbar-expand-lg navbar-light bg-white fixed-top py-3 shadow-sm">
+        <div class="container px-5">
+            <a href="" class="navbar-brand text-primary fw-bold font-monospace">studenti|spolu</a>
+            <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav m-auto ">
+                    <li class="nav-item me-3">
+                        <a class="nav-link" href="">Projekty</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link" href="">Nabídky spolupráce</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Uživatelé</a>
+                    </li>
+                </ul>
+                <!--
+                                                <a href="" class="btn btn-primary px-3 py-1 me-2">
+                                                    Přihlásit
+                                                </a>
+                                                <a href="" class="btn btn-outline-primary px-3 py-1">
+                                                    Registrace
+                                                </a>
+                -->
+
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle px-3 py-1" type="button" id="dropdownMenuButton1"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                        Jiří Andrlík
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <li><a class="dropdown-item" href="">Můj profil</a></li>
+                        <li><a class="dropdown-item" href="">Moje projekty</a></li>
+                        <li><a class="dropdown-item" href="">Žádosti o spolupráci</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="">Administrace aplikace</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">
+                                Odhlásit
+                                <span class="bi bi-box-arrow-right ms-2"></span>
+                            </a></li>
+                    </ul>
+                </div>
+
+
+            </div>
+        </div>
+    </nav>
+</header>
+
+@yield('content')
+
+<!-- Paticka -->
+<footer class="pt-4 pb-2 bg-white bg-gradient">
+    <div class="container px-5 text-center">
+
+        <div class="small text-secondary">
+            <p>&copy; 2022, Jiří Andrlík</p>
+        </div>
+    </div>
+</footer>
+
+
+<!-- Javascripty -->
+<script src="{{ asset('js/app.js') }}"></script>
+
+</body>
+</html>
+
