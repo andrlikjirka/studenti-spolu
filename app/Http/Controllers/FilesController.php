0bf3217e9572501d15b608e494457d8b612a6b9a
Jiří Andrlík <andrlik.jirka@gmail.com>
2022-04-05 00:20:23 +0200

    strana 404 soubor nenalezen

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -13,18 +13,22 @@ class FilesController extends Controller
     {
         if (Auth::check()) {
             $fileInfo = DB::select('SELECT * FROM file WHERE id_file=:id_file', [':id_file' => $id]);
-            $destinationPath = storage_path() .'\app\uploads\\';
-            $file = $destinationPath.basename($fileInfo[0]->unique_name.'.'.$fileInfo[0]->type);
-            if (file_exists($file)) {
-                // Header content type
-                header('Content-type: application/pdf');
-                header('Content-Disposition: inline; filename="' . $file . '"');
-                header('Content-Transfer-Encoding: binary');
-                header('Content-Length: ' . filesize($file));
-                header('Accept-Ranges: bytes');
-                readfile($file);
-            } else { //soubor neexistuje
-                return redirect()->route('index');
+            if (count($fileInfo) == 1) {
+                $destinationPath = storage_path() .'\app\uploads\\';
+                $file = $destinationPath.basename($fileInfo[0]->unique_name.'.'.$fileInfo[0]->type);
+                if (file_exists($file)) {
+                    // Header content type
+                    header('Content-type: application/pdf');
+                    header('Content-Disposition: inline; filename="' . $file . '"');
+                    header('Content-Transfer-Encoding: binary');
+                    header('Content-Length: ' . filesize($file));
+                    header('Accept-Ranges: bytes');
+                    readfile($file);
+                } else { //soubor neexistuje
+                    return redirect()->route('index');
+                }
+            } else {
+                abort(404, 'Soubor nenalezen');
             }
         } else {
             return redirect()->route('index');
