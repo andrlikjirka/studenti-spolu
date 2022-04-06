9e40ff4e00db8ea2ed0a63352d546782c92ffc05
Jiří Andrlík <andrlik.jirka@gmail.com>
2022-04-06 07:51:58 +0200

    ruzne formaty, prejmenovani pri stazeni

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -15,11 +15,25 @@ class FilesController extends Controller
             $fileInfo = DB::select('SELECT * FROM file WHERE id_file=:id_file', [':id_file' => $id]);
             if (count($fileInfo) == 1) {
                 $destinationPath = storage_path() .'\app\uploads\\';
-                $file = $destinationPath.basename($fileInfo[0]->unique_name.'.'.$fileInfo[0]->type);
+                $file_extension = $fileInfo[0]->type;
+                $file = $destinationPath.basename($fileInfo[0]->unique_name.'.'.$file_extension);
                 if (file_exists($file)) {
+                    switch( $file_extension )
+                    {
+                        case "pdf": $ctype="application/pdf"; break;
+                        case "txt": $ctype="application/txt"; break;
+                        case "doc": $ctype="application/doc"; break;
+                        case "xls": $ctype="application/vnd.ms-excel"; break;
+                        case "ppt": $ctype="application/vnd.ms-powerpoint"; break;
+                        case "gif": $ctype="image/gif"; break;
+                        case "png": $ctype="image/png"; break;
+                        case "jpeg":
+                        case "jpg": $ctype="image/jpg"; break;
+                        default: $ctype="application/octet-stream";
+                    }
                     // Header content type
-                    header('Content-type: application/pdf');
-                    header('Content-Disposition: inline; filename="' . $file . '"');
+                    header("Content-type: $ctype");
+                    header('Content-Disposition: inline; filename="' . $fileInfo[0]->name . '"');
                     header('Content-Transfer-Encoding: binary');
                     header('Content-Length: ' . filesize($file));
                     header('Accept-Ranges: bytes');
