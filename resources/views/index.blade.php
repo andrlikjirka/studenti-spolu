7e2ff01b51616ae9e360f8cc70fb6034007dfa77
Jiří Andrlík <andrlik.jirka@gmail.com>
2022-04-20 23:33:41 +0200

    index xxl padding

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <section class="bg-primary bg-gradient" style="padding-top: 70px">
         <div class="container px-5 py-5">
-            <div class="row gx-5 align-items-center">
+            <div class="row gx-5 align-items-center py-xxl-5">
                 <div class="col-lg-6 col-md-6 text-white">
                     <h1 class="fw-bold py-3 font-monospace">studenti|spolu<sup class=""></sup></h1>
-                    <p class="mt-3 lh-lg">
+                    <p class="mt-3 mt-xxl-4 lh-lg">
                         Tento webový informační systém je určený pro vysokoškolské studenty, kteří
                         potřebují pro své autorské projekty získat nové spolupracovníky nebo mají zájem aplikovat
                         své znalosti a dovednosti při práci na týmových projektech ostatních autorů.
